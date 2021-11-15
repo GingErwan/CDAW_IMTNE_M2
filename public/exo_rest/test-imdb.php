c8f0c5b903f7e240aeb7b159b8e78444dfe5bb78
GingErwan <48923105+GingErwan@users.noreply.github.com>
2021-11-15 11:52:57 +0100

    Implémentation modèle
    - Création d'une classe qui met en forme les films
    - Sauvegarde en local de la liste des films

--- public/exo_rest/test-imdb.php
+++ public/exo_rest/test-imdb.php
@@ -1,33 +1,78 @@
 <?php
 
-    $curl = curl_init();
 
-    $apiKey = "k_8lu5mfr5";
+    class FilmClass
+    {
+        public function __construct($title, $rank, $crew, $year)
+        {
+            $this->title = $title;
+            $this->rank = $rank;
+            $this->crew = $crew;
+            $this->year = $year;
+        }
     
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://imdb-api.com/en/API/Top250Movies/".$apiKey,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-    ));
-    
-    $json = curl_exec($curl);
-    
-    curl_close($curl);
 
-    $decodedJson = json_decode($json, true)["items"];
+        public static function getFilms()
+        {
+            if(!file_exists("list-films.json")){
+                $fileList = fopen("list-films.json", "w");
+                $curl = curl_init();
     
-    $array = '<table><tr><td>Rank</td><td>Title</td><td>Year</td><td>Crew</td></tr>';
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => "https://imdb-api.com/fr/API/Top250Movies/" . "k_8lu5mfr5",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "GET",
+                ));
+                
+                $json = curl_exec($curl);
+                file_put_contents("list-films.json", $json);
+                $listDecoded = json_decode($json, true)["items"];
+                curl_close($curl);
+            }
+            else
+            {
+                $fileList = fopen("list-films.json", "r");
+                $listDecoded = json_decode(file_get_contents("list-films.json"), true)["items"];
+            }
 
-    foreach($decodedJson as $film){
-        $array .= '<tr><td>'.$film["rank"].'</td><td>'.$film["title"].'</td><td>'.$film["year"].'</td><td>'.$film["crew"].'</td></tr>';
+            $filmArray = array();
+                
+            foreach($listDecoded as $i => $film){
+                $filmArray[$i] = new FilmClass($film["title"], $film["rank"], $film["crew"], $film["year"]);
+            };
+
+            fclose($fileList);
+            return $filmArray;
+        }
+
+        public static function showFilmsAsTable($films)
+        {
+            echo '<table><thead>
+                    <tr><th>Rank</th><th>Title</th><th>Year</th><th>Crew</th></tr>
+                </thead><tbody>';
+            foreach($films as $film)
+            {
+                echo '<tr><td>'
+                    .$film->rank.'</td><td>'
+                    .$film->title.'</td><td>'
+                    .$film->year.'</td><td>'
+                    .$film->crew.'</td></tr>';
+            }
+            echo '</tbody></table>';
+        }
+
+        public static function showAllFilmsAsTable()
+        {
+            $filmsResponse = FilmClass::getFilms();
+            static::showFilmsAsTable($filmsResponse);
+        }
+
     }
-
-    $array .= "</table>";
 
 ?>
 
@@ -51,7 +96,7 @@
 	<body>
 	<h1>Top 250 Movies</h1>
 		<?php
-			echo $array;
+			FilmClass::showAllFilmsAsTable();
 		?>
 	</body>
 </html>
